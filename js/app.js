3c0fba38f9d4f47d9993551c3db54962cf3359a9
Artem Dragunov <dragunovartem99@gmail.com>
2022-03-10 16:11:44 +0300

    Prototyping mobile nav

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
+// Actual date in footer
 const currentYear = new Date().getFullYear();
 
 const currentYearEl = document.querySelector("#current-year");
-
 currentYearEl.textContent = currentYear;
+
+// Mobile Navigation
+const mobileNavEl = document.querySelector(".header__burger");
+const headerEl = document.querySelector(".header");
+
+mobileNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("enable-mobile-nav");
+});
